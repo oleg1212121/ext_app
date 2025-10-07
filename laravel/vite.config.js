d0ea0a8115557eeb7e2ba16d93a007f0649c0521
Oleg <test@test.test>
2025-10-07 22:07:21 +0300

    Settings changes

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -4,8 +4,24 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                // 'resources/css/crossword.css',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Allow external connections (Docker)
+        port: 8000,
+        // strictPort: true,
+        hmr: {
+            host: 'localhost', // Critical for HMR in WSL2
+
+        },
+        watch: {
+            usePolling: true,
+        }
+    },
 });
